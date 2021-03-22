13e1f752690a242842d6cb8b929ef5f52ada5f50
Anastasia <asyadyoma@yandex.ru>
2021-03-22 23:57:47 +0300

    Fix a test for saving logs

--- test.py
+++ test.py
@@ -6,12 +6,10 @@ class TestGetLog(TestCase):
     """Testing of getting logs from a third-party resource,
     saving them to a local database."""
 
-    @patch('getting_logs.GetLog')
-    def test_logs_get(self, MockGetLog):
-        """Tests getting logs."""
-        logs = MockGetLog()
-
-        logs.get.return_value = {
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.data = {
             'error': '',
             'logs': [{
                 'сreated_at': '2021-01-23T12:33:14',
@@ -21,6 +19,16 @@ class TestGetLog(TestCase):
                 'user_id': '123456'
             }]
         }
+        cls.error = {
+            'error': 'created_day: does not match format 20200105'
+        }
+
+    @patch('getting_logs.GetLog')
+    def test_logs_get(self, MockGetLog):
+        """Tests getting logs."""
+        logs = MockGetLog()
+
+        logs.get.return_value = self.data
 
         response = logs.get()
         self.assertIsNotNone(response, 'Ошибка. Пустой объект.')
@@ -35,9 +43,7 @@ class TestGetLog(TestCase):
         """Tests getting logs."""
         logs = MockGetLog()
 
-        logs.get.return_value = {
-            'error': 'created_day: does not match format 20200105'
-        }
+        logs.get.return_value = self.error
 
         response = logs.get()
         self.assertIsNotNone(response, 'Ошибка. Пустой объект.')
@@ -49,16 +55,7 @@ class TestGetLog(TestCase):
     @patch('getting_logs.GetLog')
     def test_saving_logs(self, mock_saving_logs):
         """Tests saving logs."""
-        return_value = {
-            'error': '',
-            'logs': [{
-                'сreated_at': '2021-01-23T12:33:14',
-                'first_name': 'А',
-                'message': 'Write the code!',
-                'second_name': 'B',
-                'user_id': '123456'
-            }]
-        }
+        return_value = self.data
         logs = Mock(return_value)
         mock_saving_logs.return_value = logs
         self.assertIsInstance(mock_saving_logs.return_value, dict,
