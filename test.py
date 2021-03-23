d596210800b287b415c7eec0d4d3e77a58b6b82f
Anastasia <asyadyoma@yandex.ru>
2021-03-23 18:01:36 +0300

    Create a test for the correctness of the transmitted data

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+import getting_logs
+
 
 class TestGetLog(TestCase):
-    """Testing of getting logs from a third-party resource."""
+    """Testing of getting logs from a third-party resource.
+    Testing the correctness of the transmitted data
+    for saving to the database."""
 
     @classmethod
     def setUpClass(cls):
@@ -36,3 +40,35 @@ class TestGetLog(TestCase):
             response,
             logs.get.return_value,
             'Ошибка. Получены неверные данные.')
+
+    @patch('getting_logs.GetLog')
+    def test_error_get(self, MockGetLog):
+        """Tests getting logs."""
+        logs = MockGetLog()
+
+        logs.get.return_value = self.error
+
+        response = logs.get()
+        self.assertIsNotNone(response, 'Ошибка. Пустой объект.')
+        self.assertIsInstance(response, dict, 'Ошибка. Получен не json.')
+        self.assertEqual(response,
+                         logs.get.return_value,
+                         'Ошибка. Получены неверные данные.')
+
+    @patch('getting_logs.GetLog')
+    def test_saving_logs(self, MockGetLog):
+        """Tests the correctness of the transmitted data."""
+        get_log = MockGetLog()
+        get_log.get.return_value = self.data
+        logs = get_log.get()
+        get_log.saving_logs(logs)
+        self.assertEqual(
+            MockGetLog,
+            getting_logs.GetLog,
+            'Ошибка. На сохранение в базу передаются неправильные данные.')
+        self.assertIsNotNone(
+            MockGetLog.called,
+            'Ошибка. В базу ничего не сохраняется.')
+        get_log.get.assert_called_with()
+
+        get_log.saving_logs.assert_called_once_with(get_log.get.return_value)
